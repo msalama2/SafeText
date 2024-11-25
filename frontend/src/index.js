890fe60df95351e3f3476b8df900203faf4cb7ad
Marious Yousif <mariousy26@gmail.com>
2024-11-24 22:24:53 -0500

    cleaned up files and added to main

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-// src/index.js
-
-// Wait for the DOM to be fully loaded before executing the code
-document.addEventListener('DOMContentLoaded', () => {
-    // Select the 'root' div and set its inner HTML
-    const rootElement = document.getElementById('root');
-    rootElement.innerHTML = `<h1>Hello, Frontend!</h1>
-                             <p>Welcome to my website. This is a simple frontend application.</p>`;
-});
